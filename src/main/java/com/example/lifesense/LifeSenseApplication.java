96d7efe277ffc545d6682900669c5425c1a13dfc
lukasBloksa <lukassbloksa@gmail.com>
2026-03-27 18:03:19 +0100

    Initial commit

---
+++ src/main/java/com/example/lifesense/LifeSenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifesense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifesense/LifeSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
