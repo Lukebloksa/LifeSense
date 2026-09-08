7552218a3a9fde4ab4da07542c68542d693de9ea
lukasBloksa <lukassbloksa@gmail.com>
2026-09-08 20:05:23 +0200

    Adding chart.js

---
+++ client/src/components/sensorGraph/SensorGraph.tsx
@@ -0,0 +1,11 @@
+// Source - https://stackoverflow.com/q/67432014
+// Posted by Chris K.
+// Retrieved 2026-09-08, License - CC BY-SA 4.0
+
+import Chartjs from 'chart.js';
+
+function SensorGraph({ data, labels }: { data: number[]; labels: string[] }) {
+    return (
+
+    )
+}
