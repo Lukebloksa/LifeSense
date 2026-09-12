26f6553ffcfb2e79ec12ebf3a4fd97edbbdf5e80
lukasBloksa <lukassbloksa@gmail.com>
2026-09-12 18:35:27 +0200

    Finished basics of SensorGraph and SensorCard components Probably will need refactoring in the future

--- client/src/components/sensorGraph/SensorGraph.tsx
+++ client/src/components/sensorGraph/SensorGraph.tsx
@@ -5,45 +5,46 @@ import {
     YAxis,
     Tooltip,
     ResponsiveContainer,
-    CartesianGrid
+    CartesianGrid,
 } from 'recharts';
 
-const mockData = [
-    { time: '12:00', temp: 21.5 },
-    { time: '13:00', temp: 22.1 },
-    { time: '14:00', temp: 24.8 },
-    { time: '15:00', temp: 23.4 },
-    { time: '16:00', temp: 22.0 },
-];
+import styles from './SensorGraph.module.css';
 
-import styles from "./SensorGraph.module.css";
+export interface DataPoint {
+    timestamp: string;
+    value: number;
+}
 
-function SensorGraph({ data }: { data;}) {
+type SensorGraphProps = {
+    data: DataPoint[]; // Pole naměřených hodnot v čase
+    unit: string;
+    title: string;
+};
+
+function SensorGraph({ data, unit, title }: SensorGraphProps) {
     return (
         <div className={styles.graphConteiner}>
             <div className={styles.graphHeader}>
-                <span className={styles.graphTitle}>Temperature</span>
+                <span className={styles.graphTitle}>{title}</span>
             </div>
+
             <ResponsiveContainer className={styles.sensorGraphContainer} width="100%" height="100%">
-                <LineChart data={mockData} >
-                    <CartesianGrid stroke="#000000" strokeDasharray="3 3" opacity={0.4} />
-
-                    <XAxis dataKey="time" stroke="#000000" />
-                    <YAxis stroke="#000000" unit="°C" />
-
+                <LineChart data={data}>
+                    <CartesianGrid stroke="#000000" strokeDasharray="3 3" opacity={0.1} />
+                    <XAxis dataKey="timestamp" stroke="#000000" />
+                    <YAxis stroke="#000000" unit={unit} />
                     <Tooltip />
-
                     <Line
                         type="monotone"
-                        dataKey="temp"
+                        dataKey="value"
                         stroke="#31AE4D"
                         strokeWidth={2}
-                        dot={{ r: 2, fill: '#31AE4D' }}
+                        dot={{ r: 3, fill: '#31AE4D' }}
                     />
                 </LineChart>
             </ResponsiveContainer>
         </div>
-     )
+    );
 }
 
 export default SensorGraph;
