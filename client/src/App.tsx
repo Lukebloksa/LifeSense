15b369381cdb70b0cea8e60ca970cc0176b8e807
lukasBloksa <lukassbloksa@gmail.com>
2026-09-21 19:14:13 +0200

    Refactoring routing to have protected routes that cannot be open without token or registering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,18 +6,20 @@ import './App.css'
 import Dashboard from "./pages/dashboard/Dashboard";
 import LoginPage from './pages/auth/login/LoginPage'
 import RegisterPage from './pages/auth/register/RegisterPage'
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/register" replace />} />
-
                 <Route path="/login" element={<LoginPage />} />
-
-                <Route path="/dashboard" element={<Dashboard />} />
-
                 <Route path="/register" element={<RegisterPage />} />
+
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+
+                <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
         </BrowserRouter>
     )
